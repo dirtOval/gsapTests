eb73e9fbe196409a22fa88fdacf28950c69272ba
deirdre <deirdre.loyall@gmail.com>
2023-05-22 05:35:36 -0400

    box toggle can now do multiples on a delay!

--- index.js
+++ index.js
@@ -30,27 +30,40 @@ boxButton.addEventListener('click', () => {
 });
 
 //stagger test
+
+const toggleBoxes = (delay) => {
+  console.log('boxin!');
+    if (!boxesBoxed) {
+      gsap.to('.red', {
+        scale: 1.25,
+        stagger: 0.05,
+        duration: 0.2,
+        background: 'teal',
+        borderWidth: 3,
+        delay: delay
+      })
+    } else {
+      gsap.to('.red', {
+        scale: 1,
+        stagger: 0.05,
+        duration: 0.75,
+        border: 'none',
+        background: 'red',
+        ease: 'bounce.out',
+        borderWidth: 0,
+        delay: delay
+      })
+    }
+    boxesBoxed = !boxesBoxed;
+    console.log(boxesBoxed);
+}
+
 let boxesBoxed = false;
 const staggerButton = document.getElementsByClassName('stagger-button')[0];
 staggerButton.addEventListener('click', () => {
-  if (!boxesBoxed) {
-    gsap.to('.red', {
-      scale: 1.25,
-      stagger: 0.05,
-      duration: 0.2,
-      background: 'teal',
-      borderWidth: '3px'
-    })
-  } else {
-    gsap.to('.red', {
-      scale: 1,
-      stagger: 0.05,
-      duration: 0.75,
-      border: 'none',
-      background: 'red',
-      ease: 'bounce.out',
-      borderWidth: '0px'
-    })
+  const repeats = $('.repeats').val();
+  const delay = $('.delay').val();
+  for (let i = 0; i <= repeats - 1; i++) {
+    toggleBoxes(delay * i)
   }
-  boxesBoxed = !boxesBoxed;
 })
